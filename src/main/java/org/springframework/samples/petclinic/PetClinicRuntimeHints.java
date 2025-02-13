f08de959fdcaee40eb1d52dfe61cb0e2e63eaab8
Shivkumar Shivaji <sshivaji@gmail.com>
2025-02-13 12:26:54 -0800

    Update PetClinicRuntimeHints.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
@@ -29,7 +29,6 @@ public class PetClinicRuntimeHints implements RuntimeHintsRegistrar {
 		
 		hints.resources().registerPattern("db/*"); // https://github.com/spring-projects/spring-boot/issues/32654
 		hints.resources().registerPattern("messages/*");
-		hints.resources().registerPattern("help/*");
 		
         hints.resources().registerPattern("META-INF/resources/webjars/*");
 		hints.resources().registerPattern("mysql-default-conf");
